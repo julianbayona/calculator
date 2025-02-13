0bee38ff8b97ce805fbdc9ae6f9d2d1f5b16616e
julianbayona <julianbayona0315@gmail.com>
2025-02-13 09:51:38 -0500

    feat: multiplicacion

--- calculator.py
+++ calculator.py
@@ -5,4 +5,6 @@ option = int(input("1. sumar\n2. restar\n3. multiplicar\n4. dividir\n"))
 if option==1:
     print(f"el resultado de la suma es: {number_one + number_two}" )
 elif option == 2:
-    print(f"el resultado de la suma es: {number_one - number_two}" )
+    print(f"el resultado de la resta es: {number_one - number_two}" )
+elif option == 3:
+    print(f"el resultado de la multiplicacion es: {number_one * number_two}" )
