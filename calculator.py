45e5d904764fb173cb0913f0fb668219232c73ec
julianbayona <julianbayona0315@gmail.com>
2025-02-13 09:49:37 -0500

    feat: suma

---
+++ calculator.py
@@ -0,0 +1,6 @@
+number_one = int(input("digite el numero: "))
+number_two = int(input("digite el numero: "))
+option = int(input("1. sumar\n2. restar\n3. multiplicar\n4. dividir\n"))
+
+if option==1:
+    print(f"el resultado de la suma es: {number_one + number_two}" )
