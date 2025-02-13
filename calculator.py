9270a43377edf59a3c0d7d33f3a3580343d356e2
julianbayona <julianbayona0315@gmail.com>
2025-02-13 09:50:23 -0500

    feat: resta

--- calculator.py
+++ calculator.py
@@ -4,3 +4,5 @@ option = int(input("1. sumar\n2. restar\n3. multiplicar\n4. dividir\n"))
 
 if option==1:
     print(f"el resultado de la suma es: {number_one + number_two}" )
+elif option == 2:
+    print(f"el resultado de la suma es: {number_one - number_two}" )
