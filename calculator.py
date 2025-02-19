09a6d8b08a2d97dfbfa5b622d8a497368f8ed573
julianbayona <julianbayona0315@gmail.com>
2025-02-19 14:35:39 -0500

    pow_1

--- calculator.py
+++ calculator.py
@@ -1,6 +1,6 @@
 number_one = int(input("digite el numero: "))
 number_two = int(input("digite el numero: "))
-option = int(input("1. sumar\n2. restar\n3. multiplicar\n4. dividir\n"))
+option = int(input("1. sumar\n2. restar\n3. multiplicar\n4. dividir\n5. potencia"))
 
 if option==1:
     print(f"el resultado de la suma es: {number_one + number_two}" )
@@ -10,4 +10,6 @@ elif option == 3:
     print(f"el resultado de la multiplicacion es: {number_one * number_two}" )
 elif option == 4:
     print(f"el resultado de la divicion es: {number_one / number_two}" )
+elif option == 5:
+    print(f"el resultado de la potenciacion es: {number_one ** number_two}" )
 
